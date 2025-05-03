98be0937a261e40557a2a4718a936ed8b0a64c1a
scrawach <scrawach@gmail.com>
2025-05-03 03:47:55 +0300

    feature: completed floating spheres as enemies

--- src/gameplay/enemies/floating/floating_sphere.gd
+++ src/gameplay/enemies/floating/floating_sphere.gd
@@ -1,5 +1,12 @@
 class_name FloatingSphere
 extends Node3D
 
+@onready var model_player: AnimationPlayer = $View/floating_sphere/AnimationPlayer
+@onready var idle_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	model_player.seek(randf_range(0.0, 1.0))
+	idle_player.seek(randf_range(0.0, 1.0))
+
 func _on_hurt_box_area_entered(area: Area3D) -> void:
 	queue_free()
