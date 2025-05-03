7978d0ec71cebc69570411c98e862bedd527f016
scrawach <scrawach@gmail.com>
2025-05-03 20:52:43 +0300

    feature: create floating sphere material local to scene

--- src/gameplay/meta/loading.gd
+++ src/gameplay/meta/loading.gd
@@ -3,6 +3,9 @@ extends Control
 @export var artifacts: Node
 
 func _ready() -> void:
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(0.5).timeout
 	artifacts.queue_free()
-	hide()
+	
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0, 0.5)
+	tween.tween_callback(func(): hide())
