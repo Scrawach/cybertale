d254f83ecfb541f0b2e7af7af9c5bd932ecd3e30
scrawach <scrawach@gmail.com>
2025-05-04 03:33:50 +0300

    feature: spawn shop room after 2 fight rooms

--- src/gameplay/environment/gameplay_world.gd
+++ src/gameplay/environment/gameplay_world.gd
@@ -7,6 +7,7 @@ const OFFSET_BETWEEN_ROOMS: float = 50
 
 @export var fighting_room_template: PackedScene
 @export var boss_room_template: PackedScene
+@export var shoop_room_template: PackedScene
 
 @export var last_window: Control
 
@@ -18,6 +19,8 @@ var current_room: int
 var previous_room_position: Vector3
 var previous_room: ScenarioRoom
 
+var is_shop_spawned: bool
+
 func start(hero: Hero) -> void:
 	self.hero = hero
 	var room = await create_fight_room()
@@ -28,10 +31,11 @@ func start(hero: Hero) -> void:
 	await get_tree().create_timer(0.1).timeout # yep
 	progress.setup_path(number_of_rooms)
 	
-	room.start()
-	var door: NextRoomDoor = await room.completed
 	current_room += 1
 	progress.complete_room(current_room)
+	
+	room.start()
+	var door: NextRoomDoor = await room.completed
 	gameloop()
 	
 
@@ -39,22 +43,42 @@ func gameloop() -> void:
 	while current_room != (number_of_rooms + 1):
 		previous_room.stop()
 		await get_tree().create_timer(0.1).timeout
-		var room: ScenarioRoom
+		var room: ScenarioRoom = create_next_room()
+		hero.global_position = room.player_spawn.global_position
+		invoke_before_room_effects()
+		room.start()
+		previous_room.queue_free()
+		var door: NextRoomDoor = await room.completed
+		invoke_after_room_effects()
+		previous_room = room
+	last_window.visible = true
+
+func invoke_before_room_effects() -> void:
+	for effect in hero.effects:
+		effect.before_room()
+
+func invoke_after_room_effects() -> void:
+	for effect in hero.effects:
+		effect.after_room()
+
+func create_next_room() -> ScenarioRoom:
+	var room: ScenarioRoom
+	
+	if not is_shop_spawned and current_room % 2 == 0:
+		room = create_shop_room()
+		is_shop_spawned = true
+	else:
+		is_shop_spawned = false
 		
-		print(current_room)
 		if current_room == number_of_rooms:
 			room = create_boss_room()
 		else:
 			room = create_fight_room()
-		
-		hero.global_position = room.player_spawn.global_position
-		room.start()
-		previous_room.queue_free()
-		var door: NextRoomDoor = await room.completed
-		previous_room = room
+			
 		current_room += 1
 		progress.complete_room(current_room)
-	last_window.visible = true
+	
+	return room
 
 func create_fight_room() -> FightRoom:
 	return create_room(fighting_room_template)
@@ -63,6 +87,11 @@ func create_boss_room() -> BossRoom:
 	var boss_room: BossRoom = create_room(boss_room_template)
 	boss_room.initialize(hero)
 	return boss_room
+
+func create_shop_room() -> ShopRoom:
+	var shop_room: ShopRoom = create_room(shoop_room_template)
+	shop_room.initialize(hero)
+	return shop_room
 
 func create_room(template: PackedScene) -> ScenarioRoom:
 	var next_room_position: Vector3 = previous_room_position + Vector3(OFFSET_BETWEEN_ROOMS, 0, 0)
--- src/gameplay/environment/rooms/specific/shop_room.gd
+++ src/gameplay/environment/rooms/specific/shop_room.gd
@@ -1,8 +1,14 @@
 class_name ShopRoom
-extends Node3D
+extends ScenarioRoom
 
 @export var hero: Hero
 @onready var shop: Shop = $Shop
 
 func _ready() -> void:
+	if hero != null:
+		initialize(hero)
+	
+	door.used.connect(func(body: Hero, _door: NextRoomDoor): completed.emit(_door))
+
+func initialize(hero: Hero) -> void:
 	shop.initialize(hero)
